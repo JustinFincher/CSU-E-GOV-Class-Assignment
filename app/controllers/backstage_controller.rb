9678ca3b9643cf0f00b9a3c4c368dee1a773ff4f
Justin Fincher <zhtsu47@me.com>
2017-10-26 16:50:12 +0800

    Update

---
+++ app/controllers/backstage_controller.rb
@@ -0,0 +1,4 @@
+class BackstageController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
+
+  get '/',to: 'home#show'
+
+  get    'backstage',   to: 'backstage#new'
+  resources :users, path: "backstage/users"
+  resources :users, :only => [:index, :show]
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
